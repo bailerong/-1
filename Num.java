aa2864c5faa55c86b562fc9f5af6a59d91917d18
bailerong <15929276532@163.com>
2019-05-09 22:57:18 +0800

    这是我们的数字

---
+++ Num.java
@@ -0,0 +1,14 @@
+
+
+public class Num extends Token {
+    public final int value;
+
+    public Num(int v) {
+        super(Tag.NUM);
+        this.value = v;
+    }
+
+    public String toString() {
+        return  "" + value;
+    }
+}
